ca6ccc551554f7b93b74e63d863e2611c587d7c2
Chayan Neogi <136154400+Chayanethic@users.noreply.github.com>
2024-10-07 12:42:21 +0530

    Using Stack
    pura string ko iterate karo 
    jab tak stack empty na ho jaye, and stack top == 'a' && char=='b' or stack top =='c' && char == 'd' mil jaye toh stack top ko pop kardena else stack mai ohi char ko push kardena 
    return kardo stack size 
    TC -> O(n)
    SC -> O(n)

---
+++ minimumStringLength.cpp
@@ -0,0 +1,11 @@
+  int minLength(string s) {
+      stack<char> stack;
+    for (char c : s) {
+        if (!stack.empty() && (stack.top() == 'A' && c == 'B' || stack.top() == 'C' && c == 'D')) {
+            stack.pop();
+        } else {
+            stack.push(c);
+        }
+    }
+    return stack.size();
+    }
